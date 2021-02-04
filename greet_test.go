2774510ffcc9411e84ba43777e8bf51fd0426f08
Heesoo Hwang <heesoohwang1231@gmail.com>
2021-02-04 14:51:49 -0800

    Update greet_test.go

--- greet_test.go
+++ greet_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestGreet(t *testing.T) {
 	emptyResult := greet("")
-	if (emptyResult != "Hello Jenkinssss") {
+	if (emptyResult != "Hello Jenkins") {
 		t.Errorf("func greet() failed: Expected %v got %v", "Hello Jenkins", emptyResult)
 	}
 
