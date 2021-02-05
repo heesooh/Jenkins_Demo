b49a74218eb360c84c7c6bab5c7a231f2dc9d0ff
Heesoo Hwang <heesoohwang1231@gmail.com>
2021-02-04 17:03:28 -0800

    Update
    Updated the main.go and sendEmail.go to remove the errorMessage flag

--- sendEmail.go
+++ sendEmail.go
@@ -2,20 +2,29 @@ package main
 
 import (
 	"gopkg.in/gomail.v2"
+	"io/ioutil"
+	"strings"
 )
 
-func sendEmail(filename string, errorMessage string) {
-	m := gomail.NewMessage()
-	m.SetHeader("From", "blockchainwarning@omnisolu.com")
-	m.SetHeader("To", "folowal757@loopsnow.com", "blockchainwarning@omnisolu.com")
-	//m.SetAddressHeader("Cc", "dan@example.com", "Dan")
-	m.SetHeader("Subject", "Jenkins_Demo:")
-	m.SetBody("text/html", "<p>" + errorMessage + "</p>")
-	m.Attach(filename)
+func sendEmail(filename string) {
+	errMSG, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return
+	}
+	ERROR := string(errMSG)
+	if (strings.Contains(ERROR, "FAIL")) {
+		m := gomail.NewMessage()
+		m.SetHeader("From", "blockchainwarning@omnisolu.com")
+		m.SetHeader("To", "folowal757@loopsnow.com", "blockchainwarning@omnisolu.com")
+		//m.SetAddressHeader("Cc", "dan@example.com", "Dan")
+		m.SetHeader("Subject", "Jenkins_Demo:")
+		m.SetBody("text/html", "<p>" + ERROR + "</p>")
+		m.Attach(filename)
 
-	d := gomail.NewDialer("smtp.gmail.com", 587, "blockchainwarning@omnisolu.com", "01353751")
+		d := gomail.NewDialer("smtp.gmail.com", 587, "blockchainwarning@omnisolu.com", "01353751")
 
-	if err := d.DialAndSend(m); err != nil {
-		panic(err)
+		if err := d.DialAndSend(m); err != nil {
+			panic(err)
+		}
 	}
 }
